712f0849245f47eb50f23bb28fa5eafac6196153
Cowlord3001 <christophera1225@aol.com>
2019-07-31 20:25:16 -0400

    Boss 1 Tile Attacks WIP

--- Assets/Scripts/HingeTrigger.cs
+++ Assets/Scripts/HingeTrigger.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class HingeTrigger : MonoBehaviour {
 
+    HingeJoint2D MyJoint;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        MyJoint = gameObject.GetComponent<HingeJoint2D>() ;
 	}
 	
 	// Update is called once per frame
@@ -16,9 +19,16 @@ public class HingeTrigger : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && MyJoint.useMotor != true)
         {
             GetComponent<Rigidbody2D>().gravityScale = 1;
         }
+        else if (collision.gameObject.tag == "Player" && MyJoint.useMotor == true)
+        {
+            JointMotor2D x = MyJoint.motor;
+            x.motorSpeed = 1000;
+
+        }
+
     }
 }
